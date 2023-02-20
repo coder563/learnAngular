a732c4b849674aa6f881327ed30ca7d6ab0cf6a3
N M <walkerml563@gmail.com>
2023-02-20 04:49:57 +0000

    First Commit for Components Module

--- APM-Start/src/app/app.component.ts
+++ APM-Start/src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core"
 
 @Component({
-  selector: 'pm-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+
+  selector:'pm-root',
+  template:`<html><h1>{{pageTitle}}</h1>
+            <div>This is my first component</div></html>`
+
+
+
 })
-export class AppComponent {
-  title = 'Angular: Getting Started';
+export class AppComponent{
+  pageTitle:string="This is my new AppComponent"
 }
+
+ 
--- APM-Start/src/app/app.module.ts
+++ APM-Start/src/app/app.module.ts
@@ -1,15 +1,14 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { AppComponent } from "./app.component";
 
-import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  bootstrap: [AppComponent]
+  imports:[BrowserModule],
+  declarations:[AppComponent],
+  bootstrap:[AppComponent]
 })
-export class AppModule { }
+
+export class AppModule{
+ 
+}
